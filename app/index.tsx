5684f9d5250f2304a860d446dd5b31254ae1bc56
ErangaFernando404 <erangafdo1994@gmail.com>
2026-01-28 13:41:37 +0530

    tailwind Initialize

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "../global.css";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl text-blue-500">Hello NativeWind</Text>
+    </View>
+  );
+}
